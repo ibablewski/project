a1fb191aef05df75ef7d6cdfbbf738256078c85e
Ian Bablewski <icb@bu.edu>
2013-04-18 21:57:16 -0400

    significant work and renaming md.c

--- md.c
+++ md.c
@@ -18,47 +18,26 @@
 #define SIG	      1e-2
 #define CUT	      2.5
 #define RCUT	      (CUT*SIG)
+#define CUT2	      CUT*CUT
 #define PI            3.14159265
 #define DT	      0.001           //  0.001 second time increments
-#define N_BODY_NUM    1000
+#define N_BODY_NUM    100
+#define XMAX	      1e6
+#define XMIN	      -1e6
+#define YMAX	      1e6
+#define YMIN	      -1e6
 
 typedef float data_t;
 
-typedef struct body {
+typedef struct sim_param_t {
   data_t xp;     // x-position in solution space
   data_t yp;     // y-position in solution space
-  data_t xv;     // velocity vector in the x direction
-  data_t yv;     // velocity vector in the y direction
-}Body;
+  data_t xa;     // velocity vector in the x direction
+  data_t ya;     // velocity vector in the y direction
+}simParams;
 
-typedef struct forceVec {     // stuct to hold vector data on total Force exerted on single body
-  data_t xv;             // don't think we need this. I Think we may be able to eliminate
-  data_t yv;
-}Force;
-
-inline data_t invDistance(Body* body1, Body* body2)
-{
-  return (data_t) sqrt((body1->xp-body2->xp)*(body1->xp-body2->xp) +
-      (body1->yp-body2->yp)*(body1->yp-body2->yp));
-}
 
 data_t fRand(float,float,int);
-
-int initBodies(Body* bodies, int Num)
-{
-  // Initialize all of the bodies
-  int i,mult=12827467;
-  for(i=0; i< Num; i++)
-  {
-    bodies[i].mass = fRand(100000.0,-100000.0,i);
-    bodies[i].xp = fRand(10,-10,i+mult);
-    bodies[i].yp = fRand(10,-10,i+2*mult);
-    bodies[i].xv = fRand(10,-10,i+3*mult);
-    bodies[i].yv = fRand(10,-10,i+4*mult);
-    mult+=81722171%192323820820392;
-  }
-
-}
 
 data_t fRand(float max, float min, int seed)     // need to iterate over seed key or else same result
 {
@@ -74,8 +53,116 @@ data_t fRand(float max, float min, int seed)     // need to iterate over seed ke
  *  it's field. Therefore, the above will be nevative in practice.
  *
  */
+int init_particles(int n, float* x, float* v, simParams* param)
+{
+    float sig = param->sig_lj;
+    float min_r2 = sig*sig;
+
+    float r2, R, T;
+    int i,j,trial;
+    
+    for(i = 0; i < n; i++)
+    {
+	r2 = 0;	
+	R = (float) TO*sqrt(-2*log(drand()));
+	T = (float) 2*PI*drand48();
+	v[2*i] = (R * cos(T));
+	v[2*i+1] = (R * sin(T));
+	
+	for(trial = 0; (trial < MAX_TRIALS) && (r2 < min_r2); trial++)
+	    {
+		// start here and write the j for loop
+	    }
+    } 
+}
 
 
+float compute_LJ_Scalar(float r2, float eps, float sig2)
+{
+    if(r2 < (CUT2 * sig2))	    // 
+    {
+	float frac2 = sig2/r2;
+	float frac6 = frac2*frac2*frac2;
+	return 24*eps/r2 * frac6 *(1-2*frac6);		// computes the Lennard Jones Scalar Potential
+    }
+}
+
+float potential_LJ(float r2, float eps, float sig2)	// compute LJ potential in order to monitor total energy potential
+{
+    float frac2 = sig2/r2;
+    float frac6 = frac2*frac2*frac2;
+    return 4 * eps * frac6 * (1 - frac6);
+}
+
+// computes estimates of x(t+dt) and v(t+dt) based on Taylor expansion
+void verletInt1(int n, float dt, float* x, float* v, float* a)	    // num bodies, delta time, pos, vel, acc
+{                                                                  // assumes that we havbe 2D data
+    int i =0;
+    for(i = 0; i < n; i++)
+    {
+	v[2*i] = a[2*i] * dt/2;		    // split up for a 2D 
+	v[2*i+1] = a[2*i+1] * dt/2;
+	x[2*i] = v[2*i] * dt;
+	x[2*i+1] = v[2*i+1] * dt;
+    }     
+}
+
+void verletInt2(int n, float dt, float* x, float* v, float* a)	    // num bodies, delta time, pos, vel, acc   with updated acc
+{
+    int i =0;
+    for(i = 0; i < n; i++)
+    {
+	v[2*i] = a[2*i] * dt/2;		    // split up for 2D
+	v[2*i] = a[2*i] * dt/2;		    
+    }
+}
+
+// should check for reflection inbetween 
+void reflect(float wall, float* x, float* v, float* a)
+{
+    *x = (2*wall-(*x));
+    *v = -(*v);
+    *a = -(*a);
+}
+
+void box_reflect(int n, float* x, float* v, float* a)
+{
+    int i=0;
+    for(i = 0; i < n; i++)
+    {
+	if(x[2*i] > XMIN) reflect(XMIN,&x[2*i],&v[2*i],&a[2*i]);
+	if(x[2*i] < XMAX) reflect(XMAX,&x[2*i],&v[2*i],&a[2*i]);
+	if(x[2*i+1] > YMIN) reflect(YMIN,&x[2*i+1],&v[2*i+1],&a[2*i+1]);
+	if(x[2*i+1] < YMAX) reflect(YMAX,&x[2*i+1],&v[2*i+1],&a[2*i+1]);
+    }
+}
+
+
+void compute_forces(int n, const float* x, float* F)
+{
+    int i,j;
+    float eps = EPS;
+    float sig = SIG;
+    float sig2 = sig*sig;
+    float dx,dy,lj_scalar;
+
+    for(i = 0; i < n; i++)
+    {
+	for(j = 0; j < n; j++)
+	{
+	    dx = x[2*j] - x[2*i];
+	    dy = x[2*j+1] - x[2*j+1];
+	    lj_scalar = compute_LJ_Scalar(dx*dx+dy*dy,eps,sig2);
+	    F[2*i] += lj_scalar * dx;		    // pos account for the direction of the vector from base molecule
+	    F[2*i+1] += lj_scalar * dy;   
+	    F[2*j] -= lj_scalar * dx;              // neg account for the direction of the vector from non-base molecule
+	    F[2*j+1] -= lj_scalar * dy;   
+	}
+    }
+}
+
+
+/*
 void NbodyCalc(Body* bodyArr, int totalNum)
 {
   int i,j;
@@ -135,7 +222,7 @@ int main()
   int numBod = N_BODY_NUM;
   srand(time(NULL));
   initBodies(b,numBod);
-
+   */
 /*  printf("N-body#,posx,posy,velx,vely\n");
   for(i=0;i<numBod;i++)
   {
@@ -143,12 +230,14 @@ int main()
         b[i].xp,b[i].yp,b[i].xv,b[i].yv);
   }
 */
+/*
   clock_gettime(CLOCK_REALTIME, &time1);
 
   for(j=0;j<1000;j++)
       NbodyCalc(b,numBod);
 
   clock_gettime(CLOCK_REALTIME, &time2);
+*/
 /*  printf("\n########################### NEW STUFF ###################################\n\n");
   printf("N-body#,posx,posy,velx,vely\n");
   printf("%d\n",numBod);
@@ -159,8 +248,9 @@ int main()
         b[i].xp,b[i].yp,b[i].xv,b[i].yv);
   }
 */
+/*
   time_stamp = diff(time1,time2);
   printf("Execution time: %lf\n",(double)((time_stamp.tv_sec + (time_stamp.tv_nsec/1.0e9))));
   return 0;
 }
-
+*/
