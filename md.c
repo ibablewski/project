b696b2fc654c1ccc2d11b03aefec8f22b939064c
Ian Bablewski <icb@bu.edu>
2013-04-25 00:02:20 -0400

    started writing the bucket sort algorithm to place in each nbody in bucket

--- md.c
+++ md.c
@@ -15,6 +15,8 @@
 #include <errno.h>
 #include <string.h>
 
+#define LINUX       0
+
 #define EPS	      1
 #define SIG	      1e-2
 #define CUT	      2.5
@@ -33,282 +35,310 @@
 #define BOX_SIZE      1.0
 #define GRID_NUM      10.0
 
-#define GRID_BLOCK_BODIES(GRID_NUM,N_BODY_NUM) (N_BODY_NUM/GRID_NUM)	    // number of bodies in each block
-#define GRID_BLOCK_SIZE(GRID_NUM,BOX_SIZE) (BOX_SIZE/GRID_NUM)		    // size of block that contains GRID_BLOCK_NUM
+#define BLOCK_LENGTH(GRID_NUM,BOX_SIZE) (BOX_SIZE/GRID_NUM)		    // size of block that contains GRID_BLOCK_NUM
 
 
 
 typedef float data_t;
 
 typedef struct sim_param_t {
-    int npart;
-    float dt;
-    float eps_lj;
-    float sig_lj;
+  int npart;
+  float dt;
+  float eps_lj;
+  float sig_lj;
 } params;
 
 typedef struct molecule {
-    float* x;
-    float* v;
-    float* a;
-    float* F;
+  float* x;
+  float* v;
+  float* a;
+  float* F;
 }mols;
 
-typedef struct adj {
-    int* near;
-}adj;
+typedef struct adjacents {
+  int* n;
+}adjacent;
 
 
-int maxNumPartPerBlock(float blockSize, float sig)
-{
-    return ((blockSize*blockSize)/(sig*sig));
-}                                    
 
 int init_particles(int n, float* x, float* v, params* param)
 {
-    float sig = param->sig_lj;
-    float min_r2 = sig*sig;
+  float sig = param->sig_lj;
+  float min_r2 = sig*sig;
 
-    float r2,dx,dy;
-    int i,j,trial;
+  float r2,dx,dy;
+  int i,j,trial;
 
-    for(i = 0; i < n; i++)
+  for(i = 0; i < n; i++)
+  {
+    r2 = 0;	
+
+    /* Choose new point via rejection sampling */
+    for(trial = 0; (trial < MAX_TRIALS) && (r2 < min_r2); trial++)
     {
-	r2 = 0;	
+      x[2*i] = (float) (BOX_SIZE*drand48()) - BOX_SIZE/2.0;
+      x[2*i+1] = (float) (BOX_SIZE*drand48()) - BOX_SIZE/2.0;
 
-	/* Choose new point via rejection sampling */
-	for(trial = 0; (trial < MAX_TRIALS) && (r2 < min_r2); trial++)
-	{
-	    x[2*i] = (float) (BOX_SIZE*drand48()) - BOX_SIZE/2.0;
-	    x[2*i+1] = (float) (BOX_SIZE*drand48()) - BOX_SIZE/2.0;
-
-	    for(j=0; j < i; j++)
-	    {
-		dx = x[2*i] - x[2*j];
-		dy = x[2*i+1] - x[2*j+1];
-		r2 = dx*dx + dy*dy;
-		//printf("Sample%d:%d %f %f %f %f\n",i,j,min_r2,r2,dx,dy);
-		if(r2 < min_r2)
-		    break;
-	    }
-	}
-	/* If it takes too many trials, bail and declare number set up */
-	if(i > 0 && r2 < min_r2)
-	    return i;
+      for(j=0; j < i; j++)
+      {
+        dx = x[2*i] - x[2*j];
+        dy = x[2*i+1] - x[2*j+1];
+        r2 = dx*dx + dy*dy;
+        //printf("Sample%d:%d %f %f %f %f\n",i,j,min_r2,r2,dx,dy);
+        if(r2 < min_r2)
+          break;
+      }
     }
-    return n;
+    /* If it takes too many trials, bail and declare number set up */
+    if(i > 0 && r2 < min_r2)
+      return i;
+  }
+  return n;
 }
 
 void init_particles_va(int n, float* v,float* a, params* param)
 {
-    float R,T;
-    int i;
+  float R,T;
+  int i;
 
-    for(i=0; i < n; i++)
-    {
-	R = T0 * sqrt(-2.0 * log(drand48()));
-	T = 2 * PI * drand48();
-	v[2*i] = (R * cos(T));
-	v[2*i+1] = (R * sin(T));
-	//    printf("SampleVel%d %f %f\n",i,v[2*i],v[2*i+1]);
-	a[2*i] = (R * cos(T))/param->dt;
-	a[2*i+1] = (R * sin(T))/param->dt;
-    }
+  for(i=0; i < n; i++)
+  {
+    R = T0 * sqrt(-2.0 * log(drand48()));
+    T = 2 * PI * drand48();
+    v[2*i] = (R * cos(T));
+    v[2*i+1] = (R * sin(T));
+    //    printf("SampleVel%d %f %f\n",i,v[2*i],v[2*i+1]);
+    a[2*i] = (R * cos(T))/param->dt;
+    a[2*i+1] = (R * sin(T))/param->dt;
+  }
 }
 
 
 float compute_LJ_Scalar(float r2, float eps, float sig2)
 {
-    if(r2 < (CUT2 * sig2))	    // 
-    {
-	float frac2 = sig2/r2;
-	float frac6 = frac2*frac2*frac2;
-	return 24.0*eps/r2 * frac6 *(1.0-2.0*frac6);		// computes the Lennard Jones Scalar Potential
-    }
+  if(r2 < (CUT2 * sig2))	    // 
+  {
+    float frac2 = sig2/r2;
+    float frac6 = frac2*frac2*frac2;
+    return 24.0*eps/r2 * frac6 *(1.0-2.0*frac6);		// computes the Lennard Jones Scalar Potential
+  }
 }
 
 float potential_LJ(float r2, float eps, float sig2)	// compute LJ potential in order to monitor total energy potential
 {
-    float frac2 = sig2/r2;
-    float frac6 = frac2*frac2*frac2;
-    return 4.0 * eps * frac6 * (1.0 - frac6);
+  float frac2 = sig2/r2;
+  float frac6 = frac2*frac2*frac2;
+  return 4.0 * eps * frac6 * (1.0 - frac6);
 }
 
 // computes estimates of x(t+dt) and v(t+dt) based on Taylor expansion
 void verletInt1(int n, float dt, float* x, float* v, float* a)	    // num bodies, delta time, pos, vel, acc
 {                                                                  // assumes that we havbe 2D data
-    int i =0;
-    for(i = 0; i < n; i++)
-    {
-	v[2*i] = a[2*i] * dt/2.0;		    // split up for a 2D 
-	v[2*i+1] = a[2*i+1] * dt/2.0;
-	x[2*i] = v[2*i] * dt;
-	x[2*i+1] = v[2*i+1] * dt;
-    }
+  int i =0;
+  for(i = 0; i < n; i++)
+  {
+    v[2*i] = a[2*i] * dt/2.0;		    // split up for a 2D 
+    v[2*i+1] = a[2*i+1] * dt/2.0;
+    x[2*i] = v[2*i] * dt;
+    x[2*i+1] = v[2*i+1] * dt;
+  }
 }
 
 void verletInt2(int n, float dt, float* x, float* v, float* a)	    // num bodies, delta time, pos, vel, acc   with updated acc
 {
-    int v0,v1,i =0;
-    for(i = 0; i < n; i++)
-    {
-	v0 = v[2*i];
-	v1 = v[2*i+1];
-	v[2*i] = a[2*i] * dt/2.0;		    // split up for 2D
-	v[2*i] = a[2*i] * dt/2.0;
-	a[2*i] += (v[2*i]-v0)/dt;
-	a[2*i+1] += (v[2*i+1]-v1)/dt;
-    }
+  int v0,v1,i =0;
+  for(i = 0; i < n; i++)
+  {
+    v0 = v[2*i];
+    v1 = v[2*i+1];
+    v[2*i] = a[2*i] * dt/2.0;		    // split up for 2D
+    v[2*i] = a[2*i] * dt/2.0;
+    a[2*i] += (v[2*i]-v0)/dt;
+    a[2*i+1] += (v[2*i+1]-v1)/dt;
+  }
 }
 
 void computeAcc(int n, float* F, float* a)        // mass assumed to my constant and 1
 {
-    int i;
-    for(i=0; i< n; i++)
-    {
-	a[2*i] = F[2*i];
-	a[2*i+1] = F[2*i+1];
-    }
+  int i;
+  for(i=0; i< n; i++)
+  {
+    a[2*i] = F[2*i];
+    a[2*i+1] = F[2*i+1];
+  }
 }
 
 // should check for reflection inbetween 
 void reflect(float wall, float* x, float* v, float* a)
 {
-    printf("reflected!");
-    *x = (2*wall-(*x));
-    *v = -(*v);
-    *a = -(*a);
+  printf("reflected!");
+  *x = (2*wall-(*x));
+  *v = -(*v);
+  *a = -(*a);
 }
 
 void box_reflect(int n, float* x, float* v, float* a)
 {
-    int i=0;
-    for(i = 0; i < n; i++)
-    {
-	if(x[2*i] < XMIN) reflect(XMIN,&x[2*i],&v[2*i],&a[2*i]);
-	if(x[2*i] > XMAX) reflect(XMAX,&x[2*i],&v[2*i],&a[2*i]);
-	if(x[2*i+1] < YMIN) reflect(YMIN,&x[2*i+1],&v[2*i+1],&a[2*i+1]);
-	if(x[2*i+1] > YMAX) reflect(YMAX,&x[2*i+1],&v[2*i+1],&a[2*i+1]);
-    }
+  int i=0;
+  for(i = 0; i < n; i++)
+  {
+    if(x[2*i] < XMIN) reflect(XMIN,&x[2*i],&v[2*i],&a[2*i]);
+    if(x[2*i] > XMAX) reflect(XMAX,&x[2*i],&v[2*i],&a[2*i]);
+    if(x[2*i+1] < YMIN) reflect(YMIN,&x[2*i+1],&v[2*i+1],&a[2*i+1]);
+    if(x[2*i+1] > YMAX) reflect(YMAX,&x[2*i+1],&v[2*i+1],&a[2*i+1]);
+  }
 }
 
 
 void compute_forces(int n, float* x, float* F)
 {
-    int i,j;
-    float eps = EPS;
-    float sig = SIG;
-    float sig2 = sig*sig;
-    float dx,dy,lj_scalar;
+  int i,j;
+  float eps = EPS;
+  float sig = SIG;
+  float sig2 = sig*sig;
+  float dx,dy,lj_scalar;
 
-    for(i = 0; i < n; i++)
+  for(i = 0; i < n; i++)
+  {
+    for(j = 0; j < n; j++)
     {
-	for(j = 0; j < n; j++)
-	{
-	    if(i!=j){
-		dx = x[2*j] - x[2*i];
-		dy = x[2*j+1] - x[2*i+1];
-		lj_scalar = compute_LJ_Scalar(dx*dx+dy*dy,eps,sig2);
-		F[2*i] += lj_scalar * dx;     		    // pos account for the direction of the vector from base molecule
-		F[2*i+1] += lj_scalar * dy;
-		F[2*j] -= lj_scalar * dx;              // neg account for the direction of the vector from non-base molecule
-		F[2*j+1] -= lj_scalar * dy;
-	    }
-	}
+      if(i!=j){
+        dx = x[2*j] - x[2*i];
+        dy = x[2*j+1] - x[2*i+1];
+        lj_scalar = compute_LJ_Scalar(dx*dx+dy*dy,eps,sig2);
+        F[2*i] += lj_scalar * dx;     		    // pos account for the direction of the vector from base molecule
+        F[2*i+1] += lj_scalar * dy;
+        F[2*j] -= lj_scalar * dx;              // neg account for the direction of the vector from non-base molecule
+        F[2*j+1] -= lj_scalar * dy;
+      }
     }
+  }
+}
+
+
+int maxNumPartPerBlock(float blockSize, float sig)
+{
+  return ((blockSize*blockSize)/(sig*sig));
+}
+
+void gridSort(int n)
+{
+  float step = BOX_SIZE/GRID_NUM;
+  int blocks = (int) GRID_NUM*GRID_NUM;
+  int cnt[blocks];    // total # of blocks
+  int i,j,k;
+  for(i=0;i<n;i++)
+  {
+    /// need to write up some bucket sort code.
+    // must write up a check for both x and y directions to 
+    // categorize the place where the positions should
+    // be! Will most likely need to elaborate to 3D
+  }
 }
 
 int main(int argc, char** argv)
 {
-    struct timespec diff(struct timespec start, struct timespec end);
-    struct timespec time1, time2;
-    struct timespec time_stamp;
+  struct timespec diff(struct timespec start, struct timespec end);
+  struct timespec time1, time2;
+  struct timespec time_stamp;
 
 
-    int npart,i;
-    params param;
-    param.npart = N_BODY_NUM;
-    param.dt = DT;
-    param.eps_lj = EPS;
-    param.sig_lj = SIG;
+  int npart,i,Num;
 
-    mols mol;
+  params param;
+  mols mol;
+  adjacent adj;
 
-    mol.x = malloc(2*param.npart * sizeof(float));
-    mol.v = malloc(2*param.npart * sizeof(float));
-    mol.a = malloc(2*param.npart * sizeof(float));
-    mol.F = malloc(2*param.npart * sizeof(float));
+  param.npart = N_BODY_NUM;
+  param.dt = DT;
+  param.eps_lj = EPS;
+  param.sig_lj = SIG;
 
-    npart = init_particles(param.npart, mol.x , mol.v, &param);
-    if(npart < param.npart)
-    {
-	fprintf(stderr, "Could not generate %d particles, Trying %d particles instead\n",param.npart,npart);
-	param.npart = npart;
-    }
-    init_particles_va( param.npart, mol.v,mol.a, &param);
-    /*
-       for(i=0; i < param.npart; i++)
-       {
-       printf("nBody-Num: %d Posx: %f Velx: %f Accx: %f Forcex: %f\n",i,
-       mol.x[2*i],mol.v[2*i],mol.a[2*i],mol.F[2*i]);
-       printf("nBody-Num: %d Posy: %f Vely: %f Accy: %f Forcey: %f\n",i,
-       mol.x[2*i+1],mol.v[2*i+1],mol.a[2*i+1],mol.F[2*i+1]);
-       }
+  mol.x = malloc(2*param.npart * sizeof(float));
+  mol.v = malloc(2*param.npart * sizeof(float));
+  mol.a = malloc(2*param.npart * sizeof(float));
+  mol.F = malloc(2*param.npart * sizeof(float));
+
+  double Blength = BLOCK_LENGTH(GRID_NUM,BOX_SIZE);
+  Num = maxNumPartPerBlock(Blength,SIG);
+
+  adj.n = malloc(sizeof(int) * GRID_NUM * GRID_NUM * Num);
+  npart = init_particles(param.npart, mol.x , mol.v, &param);
+  if(npart < param.npart)
+  {
+    fprintf(stderr, "Could not generate %d particles, Trying %d particles instead\n",param.npart,npart);
+    param.npart = npart;
+  }
+  init_particles_va( param.npart, mol.v,mol.a, &param);
+  /*
+     for(i=0; i < param.npart; i++)
+     {
+     printf("nBody-Num: %d Posx: %f Velx: %f Accx: %f Forcex: %f\n",i,
+     mol.x[2*i],mol.v[2*i],mol.a[2*i],mol.F[2*i]);
+     printf("nBody-Num: %d Posy: %f Vely: %f Accy: %f Forcey: %f\n",i,
+     mol.x[2*i+1],mol.v[2*i+1],mol.a[2*i+1],mol.F[2*i+1]);
+     }
      */
-
-    clock_gettime(CLOCK_REALTIME, &time1);
-
+#if(LINUX)
+  clock_gettime(CLOCK_REALTIME, &time1);
+#endif
+  compute_forces(param.npart,mol.x,mol.F);
+  for(i=0;i<ITERS;i++)
+  {
+    //memset(mol.F, 0 , 2*param.npart * sizeof(float));
+    //    printf("acc10: %f\n", mol.a[i]);
+    verletInt1(param.npart,param.dt , mol.x, mol.v,mol.a);
+    box_reflect(param.npart,mol.x,mol.v,mol.a );
     compute_forces(param.npart,mol.x,mol.F);
-    for(i=0;i<ITERS;i++)
-    {
-	//memset(mol.F, 0 , 2*param.npart * sizeof(float));
-	//    printf("acc10: %f\n", mol.a[i]);
-	verletInt1(param.npart,param.dt , mol.x, mol.v,mol.a);
-	box_reflect(param.npart,mol.x,mol.v,mol.a );
-	compute_forces(param.npart,mol.x,mol.F);
-	verletInt2(param.npart,param.dt, mol.x, mol.v, mol.a);
-	//computeAcc(param.npart,mol.F,mol.a);
-	memset(mol.F, 0 , 2*param.npart * sizeof(float));
-	//printf("acc10: %f\n", mol.a[i]);
-    }
-    clock_gettime(CLOCK_REALTIME, &time2);
+    verletInt2(param.npart,param.dt, mol.x, mol.v, mol.a);
+    //computeAcc(param.npart,mol.F,mol.a);
+    memset(mol.F, 0 , 2*param.npart * sizeof(float));
+    //printf("acc10: %f\n", mol.a[i]);
+  }
+#if(LINUX)
+  clock_gettime(CLOCK_REALTIME, &time2);
+#endif
+  /*
+     for(i=0; i < param.npart; i++)
+     {
+     printf("nBody-Num: %d Posx: %f Velx: %f Accx: %f Forcex: %f\n",i,
+     mol.x[2*i],mol.v[2*i],mol.a[2*i],mol.F[2*i]);
+     printf("nBody-Num: %d Posy: %f Vely: %f Accy: %f Forcey: %f\n",i,
+     mol.x[2*i+1],mol.v[2*i+1],mol.a[2*i+1],mol.F[2*i+1]);
+     }
+     */
+#if(LINUX)
+  double blength = BLOCK_LENGTH(GRID_NUM,BOX_SIZE);
+  printf("Boxsize: %lf,Blocksize: %lf,MaxBodiesPerBlock: %d\n",BOX_SIZE,blength, maxNumPartPerBlock(blength,SIG));
+  time_stamp = diff(time1,time2);
+  printf("Execution time: %lf\n",(double)((time_stamp.tv_sec + (time_stamp.tv_nsec/1.0e9))));
+#else
+  double blength = BLOCK_LENGTH(GRID_NUM,BOX_SIZE);
+  printf("Boxsize: %lf,Blocksize: %lf,MaxBodiesPerBlock: %d\n",BOX_SIZE,blength, maxNumPartPerBlock(blength,SIG));
 
-    /*
-       for(i=0; i < param.npart; i++)
-       {
-       printf("nBody-Num: %d Posx: %f Velx: %f Accx: %f Forcex: %f\n",i,
-       mol.x[2*i],mol.v[2*i],mol.a[2*i],mol.F[2*i]);
-       printf("nBody-Num: %d Posy: %f Vely: %f Accy: %f Forcey: %f\n",i,
-       mol.x[2*i+1],mol.v[2*i+1],mol.a[2*i+1],mol.F[2*i+1]);
-       }
-     */  
-    double bods = GRID_BLOCK_BODIES(GRID_NUM,BOX_SIZE);
-    double num = GRID_BLOCK_SIZE(GRID_NUM,N_BODY_NUM);
-    printf("Boxsize: %lf,Blocksize: %lf,Bodies: %lf, %d\n",num, BOX_SIZE,bods, maxNumPartPerBlock(0.1,SIG));
-    time_stamp = diff(time1,time2);
-    printf("Execution time: %lf\n",(double)((time_stamp.tv_sec + (time_stamp.tv_nsec/1.0e9))));
+#endif
 
-    
-    free(mol.x);
-    free(mol.v);
-    free(mol.a);
-    free(mol.F);
-    printf("Done\n");
+  free(adj.n);
+  free(mol.x);
+  free(mol.v);
+  free(mol.a);
+  free(mol.F);
+  printf("Done\n");
 
-    return 0;
+  return 0;
 }
 
 struct timespec diff(struct timespec start, struct timespec end)
 {
-    struct timespec temp;
-    if ((end.tv_nsec-start.tv_nsec)<0) {
-	temp.tv_sec = end.tv_sec-start.tv_sec-1;
-	temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
-    } else {
-	temp.tv_sec = end.tv_sec-start.tv_sec;
-	temp.tv_nsec = end.tv_nsec-start.tv_nsec;
-    }
-    return temp;
+  struct timespec temp;
+  if ((end.tv_nsec-start.tv_nsec)<0) {
+    temp.tv_sec = end.tv_sec-start.tv_sec-1;
+    temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+  } else {
+    temp.tv_sec = end.tv_sec-start.tv_sec;
+    temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+  }
+  return temp;
 }
 
 
